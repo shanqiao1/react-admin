fdc28b4ea404f48dc1649a0b2886936d67e3d6c7
shanqiao <981778398@qq.com>
2019-02-28 15:30:16 +0800

    完成左侧静态导航

--- src/components/left-nav/index.jsx
+++ src/components/left-nav/index.jsx
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import {Menu} from 'antd';
+import {Menu,Icon} from 'antd';
 import './index.less';
 import logo from '../../assets/images/logo.png';
 
@@ -16,11 +16,42 @@ export default class LeftNav extends Component{
                     <img src={logo} alt="logo"/>
                     <h2>后台程序</h2>
                 </header>
-                <Menu>
-                    <Item>首页</Item>
-                    <SubMenu>
-                        <Item>品类管理</Item>
-                        <Item>商品管理</Item>
+                <Menu mode="inline" theme="dark">
+                    <Item>
+                        <Icon type="home" />
+                        <span>首页</span>
+                    </Item>
+                    <SubMenu title={<span><Icon type="home" />商品</span>}>
+                        <Item>
+                            <Icon type="bars" />
+                            <span> 品类管理</span>
+
+                    </Item>
+                        <Item>
+                            <Icon type="home" />
+                            <span> 商品管理</span>
+
+                        </Item>
+                    </SubMenu>
+                    <Item>
+                        <Icon type="user" />
+                        <span>用户管理</span>
+                    </Item>
+                    <Item>
+                        <Icon type="safety" />
+                        <span>权限管理</span>
+                    </Item>
+                    <SubMenu title={<span><Icon type="home" />图形图表</span>}>
+                        <Item>
+                            <Icon type="home" />
+                            <span> 品类管理</span>
+
+                        </Item>
+                        <Item>
+                            <Icon type="home" />
+                            <span> 商品管理</span>
+
+                        </Item>
                     </SubMenu>
                 </Menu>
 
